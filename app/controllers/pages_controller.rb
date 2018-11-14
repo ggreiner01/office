d96592f4046daf394933d4a5b0e47147971e1ff5
Elijah <ejoebanks@gmail.com>
2018-11-14 09:48:25 -0600

    Change column name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,11 @@ class PagesController < ApplicationController
     @orders = Order.all
     @costs = Cost.all
     @inventories = Inventory.all
-    @supplyQuantity = Supply.joins(:inventories)
-           .select('inventories.supply_id,supplies.supplyname,inventories.quantity, supplies.suppliesid')
+    #@supplyQuantity = Supply.joins(:inventories)
+    #       .select('inventories.supply_id,supplies.supplyname,inventories.quantity, supplies.suppliesid')
     @orderInfo = Order.joins(:suppliers).where('orders.suppliersid = suppliers.id').select('orders.*, suppliers.name')
 
-    @osInfo = Order.joins("INNER JOIN suppliers ON suppliers.id = orders.supplier_id INNER JOIN supplies ON supplies.suppliesid = orders.suppliesid INNER JOIN users ON orders.empid = users.id").select('users.first_name, users.last_name, orders.*', 'suppliers.name', 'supplies.*')
+    @osInfo = Order.joins("INNER JOIN suppliers ON suppliers.id = orders.supplier_id INNER JOIN supplies ON supplies.suppliesid = orders.supply_id INNER JOIN users ON orders.employee_id = users.id").select('users.first_name, users.last_name, orders.*', 'suppliers.name', 'supplies.*')
 
     @oInfo = Supplier.joins("INNER JOIN costs ON costs.supplier_id = suppliers.id INNER JOIN supplies ON supplies.suppliesid = costs.supply_id").select("supplies.supplyname, suppliers.name, costs.*")
     @oz = OrderItem.joins("INNER JOIN costs ON costs.supply_id = order_items.supply_id").select('costs.cost AS price, order_items.supply_id').where('costs.supply_id = 5');
